abeaf931dbbec6c4422a65795741881510e2ba73
Tsharun <harun.welltouch@gmail.com>
2026-04-11 16:46:52 +0600

    fix styling

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,14 @@
 import "./globals.css";
-import AuthProvider from "./context/AuthContext";
-import Navbar from "./components/Navbar";
+
+export const metadata = {
+  title: "ShebaSathi",
+  description: "Patient Care Service",
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="bn">
-      <body>
-        <AuthProvider>
-          <Navbar />
-          {children}
-        </AuthProvider>
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
