10407cd60a35a1907f4ae7d6c80deb5e8ca2b343
Tsharun <harun.welltouch@gmail.com>
2026-04-13 00:04:26 +0600

    Update again

--- app/layout.js
+++ app/layout.js
@@ -2,14 +2,24 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import { AuthProvider } from "../context/AuthContext";
 
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body>
+//         <AuthProvider>
+//           <Navbar />
+//           {children}
+//         </AuthProvider>
+//       </body>
+//     </html>
+//   );
+// }
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <AuthProvider>
-          <Navbar />
-          {children}
-        </AuthProvider>
+    <html lang="bn">
+      <body suppressHydrationWarning={true}>
+        {children}
       </body>
     </html>
   );
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,19 +3,29 @@
 import Link from "next/link";
 
 export default function Navbar() {
+  const user =
+    typeof window !== "undefined"
+      ? JSON.parse(localStorage.getItem("user"))
+      : null;
+
   return (
-    <div className="bg-white shadow px-8 py-4 flex justify-between items-center">
+    <div className="bg-white shadow px-8 py-4 flex justify-between">
+      <h1 className="font-bold text-xl">ShebaSathi</h1>
 
-      <div className="flex items-center gap-2">
-        <img src="/logo.png" className="h-8" />
-        <span className="font-bold text-xl">ShebaSathi</span>
-      </div>
-
-      <div className="flex gap-5">
+      <div className="flex gap-4">
         <Link href="/">হোম</Link>
         <Link href="/doctors">ডাক্তার</Link>
-        <Link href="/login">লগইন</Link>
-        <Link href="/register">রেজিস্টার</Link>
+
+        {user ? (
+          <>
+            <Link href="/dashboard">ড্যাশবোর্ড</Link>
+          </>
+        ) : (
+          <>
+            <Link href="/login">লগইন</Link>
+            <Link href="/register">রেজিস্টার</Link>
+          </>
+        )}
       </div>
     </div>
   );
